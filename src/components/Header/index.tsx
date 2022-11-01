163b93a223767f4eb349b58e74cd7dc05c3fc615
kevin <kevinalves.js@gmail.com>
2022-10-31 21:57:35 -0300

    styles: add newTask input style
    - Also: installed react-icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,20 @@
 import todoLogo from '../../assets/logo.svg'
 import styles from "./header.module.css"
+import { AiOutlinePlusCircle } from "react-icons/ai"
+
 export function Header() {
     return (
-        <header className={styles.header}>
-            <img src={todoLogo} alt="" />
-        </header>
+        <>
+            <header className={styles.header}>
+                <img src={todoLogo} alt="" />
+                <form action="" className={styles.newTaskForm}>
+                    <input type="text" placeholder='Adicione uma nova tarefa' />
+                    <button>
+                        Criar
+                        <AiOutlinePlusCircle size={20} />
+                    </button>
+                </form>
+            </header>
+        </>
     )
 }
