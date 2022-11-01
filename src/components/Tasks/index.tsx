29af1743a67ce09e5dda0d8750b05a81358b5f69
kevin <kevinalves.js@gmail.com>
2022-10-31 22:18:12 -0300

    style: add css of task section header

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -1,15 +1,15 @@
 import styles from "./tasks.module.css"
 
-export function Task() {
+export function Tasks() {
     return (
-        <section>
+        <section className={styles.tasks}>
             <header>
                 <div>
                     <p>Tarefas criadas</p>
                     <span>8</span>
                 </div>
                 <div>
-                    <p>Concluidas</p>
+                    <p className={styles.textPurple}>Concluidas</p>
                     <span>5 de 8</span>
                 </div>
             </header>
